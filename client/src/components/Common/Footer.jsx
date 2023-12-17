a77656032338aca8015aab7575a1f0749b99b0fb
Julie Ivanova <66208546+tiny4ngel@users.noreply.github.com>
2023-12-17 20:05:26 +0200

    general improvements

--- client/src/components/Common/Footer.jsx
+++ client/src/components/Common/Footer.jsx
@@ -1,12 +1,12 @@
 import Path from "../../paths";
-
+import { Link } from "react-router-dom";
 
 export default function Footer(){
     return(
         <footer>
         <div className="footer-content">
             <div className="footer-nav">
-                <a href={Path.Home}>Ladies Loot - Fashion for Girl Gamers</a>
+                <Link to={Path.Home}>Ladies Loot - Fashion for Girl Gamers</Link>
             </div>
             <img src="images/pixel.png" alt="Pixel Heart" className="footer-heart"/>
             <p>Project created for the SoftUni React.js Course, 2023</p>
